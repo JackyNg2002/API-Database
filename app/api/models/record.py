da20f825ac9f11f9149f0064594a1fea44e285ce
ken20020209 <94442724+ken20020209@users.noreply.github.com>
2024-04-24 15:11:03 +0800

    test

---
+++ app/api/models/record.py
@@ -0,0 +1,50 @@
+from ..models import db
+from datetime import datetime
+
+class RecordModel(db.Model):
+    __tablename__ = 'records'
+
+    id = db.Column(db.Integer, primary_key=True,nullable=False,autoincrement=True)
+
+    robot_id = db.Column(db.Integer, db.ForeignKey('robots.id'), nullable=False)
+    robot = db.relationship('RobotModel', backref=db.backref('robots', lazy='select'))
+    datetime = db.Column(db.DateTime, default=datetime.now, nullable=False)
+    name = db.Column(db.String(255), nullable=False, unique=True)
+
+    def add_record(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update_record(self):
+        db.session.commit()
+    
+    def delete_record(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'robot_id': self.robot_id,
+            'datetime': self.datetime,
+            'name': self.name
+        }
+    
+    @classmethod
+    def find_by_name(cls, name):
+        return db.session.execute(db.select(cls).filter_by(name = name)).first()
+    
+    @classmethod
+    def get_all_record(cls,robot_id=None,limit=None,offset=None):
+        query = db.select(cls)
+        if robot_id:
+            query = query.filter_by(robot_id = robot_id)
+        if limit:
+            query = query.limit(limit)
+        if offset:
+            query = query.offset(offset)
+        return db.session.execute(query).all()
+    
+    @classmethod
+    def find_by_id(cls, id):
+        return db.session.execute(db.select(cls).filter_by(id = id)).first()
---
+++ app/api/resources/user.py
@@ -0,0 +1,105 @@
+import uuid
+from flask_restful import Resource,reqparse
+from flask_jwt_extended import jwt_required
+from werkzeug.security import generate_password_hash
+from ..models.user import UserModel
+from ..models.permission import PermissionModel
+from ..common.utils import res
+
+class UserService(Resource):
+    @jwt_required()
+    def get(self):
+        users = UserModel.get_all_user()
+        result=[]
+        for user in users:
+            result.append(user.dict())
+        return res(data=result, message="Users retrieved successfully")
+    
+    @jwt_required()
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True, help='id is required',location='json')
+        data = parser.parse_args()
+
+        user_id = data['id']
+        if(user_id is None):
+            return res(message="id is required", code="-1",status=400)
+        
+        user_tuple = UserModel.find_by_id(user_id)
+        if(user_tuple is None):
+            return res(message="User not found", code="-1",status=404)
+        (user,)=user_tuple
+        permission=PermissionModel.find_by_user_id(user_id)
+        if permission:
+            for p in permission:
+                p.delete_permission()
+        user.delete_user()
+
+        return res(message="User deleted successfully")
+
+    @jwt_required()
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True, location='json')
+        parser.add_argument('password', type=str, required=True, dest='pwd', location='json')
+        parser.add_argument('email', type=str, required=True, location='json')
+        parser.add_argument('role', type=str, required=True, location='json')
+
+
+        data = parser.parse_args()
+
+        # valid email formate
+        if not UserModel.valid_email(data['email']):
+            return res(data=None, message="Invalid email format", code="-1", status=400)
+        
+        # valid password length > 6
+        if len(data['pwd']) < 6:
+            return res(data=None, message="Password length must be greater than 6", code="-1", status=400)
+
+
+        if UserModel.find_by_username(data['username']):
+            return res(data=None, message="User {} already exists".format(data['username']), code="-1", status=400)
+        else:
+            try:
+                data['salt']=uuid.uuid4().hex
+                data['pwd'] = generate_password_hash('{}{}'.format(data['salt'], data['pwd']))
+                user = UserModel(**data)
+                user.add_user()
+                return res(data=None, message="User {} created successfully".format(data['username']))
+            except Exception as e:
+                return res(data=None, message="An error occurred while creating the user", code="-1", status=500)       
+
+
+    @jwt_required()
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True, help='id is required',location='json')
+        parser.add_argument('username', type=str, required=False, location='json')
+        parser.add_argument('password', type=str, required=False, dest='pwd', location='json')
+        parser.add_argument('email', type=str, required=False, location='json')
+        parser.add_argument('role', type=str, required=False, location='json')
+
+        data = parser.parse_args()
+
+        user_id = data['id']
+        if(user_id is None):
+            return res(message="id is required", code="-1",status=400)
+        
+        user_tuple = UserModel.find_by_id(user_id)
+        if(user_tuple is None):
+            return res(message="User not found", code="-1",status=404)
+        (user,)=user_tuple
+
+        if(data['username'] is not None):
+            user.username = data['username']
+        if(data['pwd'] is not None):
+            user.salt=uuid.uuid4().hex
+            user.pwd = generate_password_hash('{}{}'.format(user.salt, data['pwd']))
+        if(data['email'] is not None):
+            user.email = data['email']
+        if(data['role'] is not None):
+            user.role = data['role']
+
+        user.update_user()
+
+        return res(message="User edited successfully")
